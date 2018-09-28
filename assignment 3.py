bb293c997fd8724b697e17fd4f427090a6f8d7d6
noviayou <35281003+noviayou@users.noreply.github.com>
2018-09-27 21:28:32 -0500

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 24 14:45:20 2018
+
+@author: noviayou
+"""
+
+#assignement 3
+import pandas as pd
+
+df = pd.read_csv('link.csv')
+newdf = df.dropna(subset=['ebit'])
+newdf1 = newdf.select_dtypes(include = ['number'])
+x = newdf1.drop('ebit',axis = 1)
+y = newdf1.ebit
+
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(max_leaf_nodes = 4)#random_state get same result
+ #mean squared error/varianve reduction
+regressor.fit(x, y)
+
+##########
+
+header = newdf1.columns.values
+
+
+
+
+def is_numeric(value):
+    """Test if a value is numeric."""
+    return isinstance(value, int) or isinstance(value, float)
+
+class Question:
+    """A Question is used to partition a dataset.
+
+    This class just records a 'column number' (e.g., 0 for Color) and a
+    'column value' (e.g., Green). The 'match' method is used to compare
+    the feature value in an example to the feature value stored in the
+    question. See the demo below.
+    """
+#value is threshold
+#column the one to be splitted
+    def __init__(self, column, value):  #all class 
+        self.column = column
+        self.value = value
+
+    def match(self, example): #
+        # Compare the feature value in an example to the
+        # feature value in this question.
+        val = example[self.column]
+        if is_numeric(val):
+            return val >= self.value
+        else:
+            return val == self.value
+
+    def __repr__(self):
+        # This is just a helper method to print
+        # the question in a readable format.
+        condition = "=="
+        if is_numeric(self.value):
+            condition = ">="
+        return "Is %s %s %s?" % (
+            header[self.column], condition, str(self.value))  #问句function
+        
+# note how to return more than one value in a function
+def partition(rows, question):
+    """Partitions a dataset.
+
+    For each row in the dataset, check if it matches the question. If
+    so, add it to 'true rows', otherwise, add it to 'false rows'.
+    """
+    true_rows, false_rows = [], []
+    for row in rows:
+        if question.match(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+    return true_rows, false_rows
+
+def gini(rows):
+    """Calculate the Gini Impurity for a list of rows.
+
+    There are a few different ways to do this, I thought this one was
+    the most concise. See:
+    https://en.wikipedia.org/wiki/Decision_tree_learning#Gini_impurity
+    """
+    counts = class_counts(rows)
+    impurity = 1
+    for lbl in counts:
+        prob_of_lbl = counts[lbl] / float(len(rows))
+        impurity -= prob_of_lbl**2
+    return impurity
+
+def info_gain(left, right, current_uncertainty):
+    """Information Gain.
+
+    The uncertainty of the starting node, minus the weighted impurity of
+    two child nodes.
+    """
+    p = float(len(left)) / (len(left) + len(right))
+    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)     
+    
+
+def find_best_split(rows):
+    """Find the best question to ask by iterating over every feature / value
+    and calculating the information gain."""
+    best_gain = 0  # keep track of the best information gain
+    best_question = None  # keep train of the feature / value that produced it
+    current_uncertainty = gini(rows)
+    n_features = len(rows[0]) - 1  # number of columns
+
+    for col in range(n_features):  # for each feature
+
+        values = set([row[col] for row in rows])  # unique values in the column
+
+        for val in values:  # for each value
+
+            question = Question(col, val)
+
+            # try splitting the dataset
+            true_rows, false_rows = partition(rows, question)
+
+            # Skip this split if it doesn't divide the
+            # dataset.
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+
+            # Calculate the information gain from this split
+            gain = info_gain(true_rows, false_rows, current_uncertainty)
+
+            # You actually can use '>' instead of '>=' here
+            # but I wanted the tree to look a certain way for our
+            # toy dataset.
+            if gain >= best_gain:
+                best_gain, best_question = gain, question
+
+    return best_gain, best_question
+
+#should use variance of each of Y and split in lower variance instead of using  
+
+##Steps to calculate Variance:
+#
+#Calculate variance for each node.
+#Calculate variance for each split as weighted average of each node variance.
+        
+        
+        
+        
